d272adfbab9cb3bdb0e751767ee83fb045088bb1
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-12-05 10:31:32 -0300

    Project Base

---
+++ src/test/java/com/rodrigoajx/inspection_map_user_service/InspectionMapUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigoajx.inspection_map_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectionMapUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
