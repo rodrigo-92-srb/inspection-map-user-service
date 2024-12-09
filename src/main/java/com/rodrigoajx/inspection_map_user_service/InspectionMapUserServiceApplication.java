52fbbf3d36ddb88281c9b2cb31bad321e1fa3857
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-12-09 16:46:54 -0300

    Project Base

---
+++ src/main/java/com/rodrigoajx/inspection_map_user_service/InspectionMapUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigoajx.inspection_map_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InspectionMapUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InspectionMapUserServiceApplication.class, args);
+	}
+
+}
